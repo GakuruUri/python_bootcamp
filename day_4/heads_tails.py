98369013f61a8a9ba2c83c99f23de8eaaef5a690
GakuruUri <urigakuru@icloud.com>
2024-05-04 14:56:35 +0300

    Day  of 100 days of code

---
+++ day_4/heads_tails.py
@@ -0,0 +1,16 @@
+import random
+
+
+random_side = random.randint(0, 1)
+if random_side == 1:
+    print("Heads.")
+else:
+    print("Tails.")
+
+
+# coin_toss = random.randint(1, 2)
+
+# if coin_toss == 1:
+#     print("heads.".title())
+# else:
+#     print("tails.".title())
