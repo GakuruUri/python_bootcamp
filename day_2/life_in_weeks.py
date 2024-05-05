89cf37e552dce47e2f1e7f64572a29544d797990
GakuruUri <urigakuru@icloud.com>
2024-05-05 11:49:00 +0300

    Day 2 of 100 days of code.

--- day_2/life_in_weeks.py
+++ day_2/life_in_weeks.py
@@ -1,3 +1,11 @@
+age = input("Please enter your current age: ")
+years = 99 - int(age)
+
+age_in_weeks = years * 52
+print(f"You have {age_in_weeks} weeks left.")
+
+
+
 age = input("Enter your current age:")
 
 years = 90 - int(age)
