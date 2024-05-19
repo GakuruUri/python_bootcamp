f3ee5491eec980df4e19f94289cf0c8fe2020894
GakuruUri <urigakuru@icloud.com>
2024-05-19 20:37:09 +0300

    Day 16 of 100 days of code.

---
+++ day_16/turtle_start.py
@@ -0,0 +1,14 @@
+# import turtle
+#
+# timmy = turtle.Turtle()
+
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+
+
+my_screen = Screen()
+print(my_screen.canvheight, my_screen.canvwidth)
+
+
+
