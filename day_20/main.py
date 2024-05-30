9e7eab518e1c2759c431b181d7f382075ff7fab0
GakuruUri <urigakuru@icloud.com>
2024-05-30 19:18:22 +0300

    Day 20 of 100 days of code.

--- day_20/main.py
+++ day_20/main.py
@@ -27,9 +27,12 @@ while game_is_on:
     # for seg in segments:
     #     seg.forward(20)
 
-    for seg_num in range(start=, stop=, step=):
-        segments[seg_num - 1]
-        segments[seg_num].goto()
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+
+    segments[0].forward(20)
 
 
 
