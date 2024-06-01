e50b36760175fa0e508ebeb174e5eb6ab3967280
GakuruUri <urigakuru@icloud.com>
2024-06-01 22:16:46 +0300

    Day 21 of 100 days of code.

--- day_20/main.py
+++ day_20/main.py
@@ -1,15 +1,25 @@
+"""
+1. Create a snake body
+2. Move the snake
+3. Detect Collision with food.
+4. Create a scoreboard
+5. Detect collision with wall
+6. Detect collision with tail
+"""
+
 from turtle import Screen
 from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 import time
 
+## TODO 1: Create a snake body
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My snake game")
 screen.tracer(0)
-
 
 # starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 #
@@ -26,13 +36,12 @@ snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
-
+## TODO 2: Move the snake
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-
 
 game_is_on = True
 while game_is_on:
@@ -41,11 +50,12 @@ while game_is_on:
     # for seg in segments:
     #     seg.forward(20)
     snake.move()
-
-    # Detect collision with food
+## TODO 3: Detect collision with food
     if snake.head.distance(food) < 15:
         #   print("nom nom nom")
         food.refresh()
+        snake.extend()
+## TODO 4: Create a scoreboard
         scoreboard.increase_score()
 
     # for seg_num in range(len(segments) - 1, 0, -1):
@@ -54,15 +64,21 @@ while game_is_on:
     #     segments[seg_num].goto(new_x, new_y)
     #
     # segments[0].forward(20)
+## TODO 5: Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+
+## TODO 6: Detect collision with tail.
+    for segment in snake.segments[1:]:
+        # if segment == snake.head:
+        #     pass
+
+        # if head collides with any segment in the tail:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
 
 screen.exitonclick()
-
-
-"""
-1. Create a snake body
-2. Move the snake
-3. Detect Collision with food.
-4. Create a scoreboard
-5. Detect collision with wall
-6. Detect collision with tail
-"""
--- day_20/scoreboard.py
+++ day_20/scoreboard.py
@@ -11,12 +11,16 @@ class Scoreboard(Turtle):
         self.score = 0
         self.color("white")
         self.penup()
-        self.goto(0, 270)
+        self.goto(0, 265)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
