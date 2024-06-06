4e71b0a77bec9ab72ef4b144c3265c08a9d61db6
GakuruUri <urigakuru@icloud.com>
2024-06-06 14:15:27 +0300

    Day 22 of 100 days of code.

--- day_22/main.py
+++ day_22/main.py
@@ -11,6 +11,8 @@
 
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 
@@ -26,6 +28,10 @@ screen.tracer(0)    # Turns off animation where the paddle is first drawn in the
 ##TODO 3: Create another paddle.
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+
+##TODO 4: Create the ball and make it move.
+ball = Ball()
+scoreboard = Scoreboard()
 
 ##TODO 2: Create and move a paddle.
 # paddle = Turtle()
@@ -55,6 +61,29 @@ screen.onkey(l_paddle.go_down, "s")
 # Reactivates the paddle movement after paddle is created and animation stopped using tracer method.
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+##TODO 5: Detect collision with ball and bounce.
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # Need ball to bounce
+        ball.bounce_y()
+
+
+##TODO 6: Detect collision with paddle.
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+
+##TODO 7: Detect collision when paddle misses.
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
 
 screen.exitonclick()
