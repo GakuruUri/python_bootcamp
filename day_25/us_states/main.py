06815f71301a61e52dffa29ee1189b076ec09a25
GakuruUri <urigakuru@icloud.com>
2024-06-09 16:30:08 +0300

    Day 25 of 100 days of code.

--- day_25/us_states/main.py
+++ day_25/us_states/main.py
@@ -1,22 +1,44 @@
 import turtle
 import pandas
 
-data = pandas.read_csv("50_states.csv")
-
-
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 turtle.addshape(image)
 turtle.shape(image)
 
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-for state in answer_state:
-    if state in data:
-        print("Hurray")
-    else:
-        print("Nay")
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state in all_states:
+                missing_states.append(state)
+        # print(missing_states)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("States_to_learn.csv")
+
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+
+
+
+
 
 
 # Getting coordinates on the image of states.
@@ -26,4 +48,4 @@ for state in answer_state:
 # turtle.mainloop()
 #
 #
-# # screen.exitonclick()
+# screen.exitonclick()
