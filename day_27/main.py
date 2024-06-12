0e18e3ec48be91769a0a01195ad934a4b1279e27
GakuruUri <urigakuru@icloud.com>
2024-06-12 21:46:21 +0300

    Day 27 of 100 days of code.

--- day_27/main.py
+++ day_27/main.py
@@ -1,14 +1,38 @@
-import tkinter
+# import tkinter
+from tkinter import *  # Import everything so that you eliminate the use of keyword tkinter
 
-window = tkinter.Tk()
+# window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI program")
 window.minsize(500, 300)
 
 # Label
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+# my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
+# my_label.pack(side="left")
+my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
 
 
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+    # my_label.config(text="Button Got Clicked.")
+
+
+# Create a button
+# button = tkinter.BU
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+
+# Entry
+
+input = Entry(width=10)
+input.pack()
 
 
 window.mainloop()
