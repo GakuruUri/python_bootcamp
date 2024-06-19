670956a1b06eb9b3f99f39ae2f4c5d86fa8db80c
GakuruUri <urigakuru@icloud.com>
2024-06-19 12:47:20 +0300

    Day 30 of 100 days of code.

--- day_29_password/main.py
+++ day_29_password/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -55,21 +56,37 @@ def save():
     website = website_input.get()
     email = email_username_input.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any field empty.")
-
+    # See the password_manager.py file for redacted code.
     else:
+        try:
+            with open("data.json", "r") as data_file:
+                # read old data
+                data = json.load(data_file)
 
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"These are the details entered: \nEmail: {email} \nPassword: "
-                                               f"{password} \nIs it okay to save?")
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
 
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_input.delete(0, END)
-                password_entry.delete(0, END)
+        else:
+            # Update old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                # Saving old data
+                json.dump(data, data_file, indent=4)
+
+        finally:
+            website_input.delete(0, END)
+            password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
