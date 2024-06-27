08e55110c26796ebb2ab4b4b4f51f9b3870d23f9
GakuruUri <urigakuru@icloud.com>
2024-06-27 21:36:04 +0300

    Day 32 of 100 days of code.

--- day_32/main.py
+++ day_32/main.py
@@ -1,6 +1,31 @@
+import smtplib
+import datetime as dt
+import random
+
+MY_EMAIL = "gakurufam@gmail.com"
+MY_PASSWORD = "zaovrfhjzsjjlbva"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+
+if weekday == 3:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"Subject: Monday Motivation\n\n{quote}"
+        )
+
 # import smtplib
 #
-# my_email = "gakurufam@gmail.com"
+# my_email = "ggmaakurufam@il.com"
 # password = "zaov rfhj zsjj lbva"
 #
 # with smtplib.SMTP("smtp.gmail.com") as connection:
@@ -11,8 +36,13 @@
 #
 
 
-import datetime as dt
-
-now = dt.datetime.now()
-year = now.year
-print(now)
+# import datetime as dt
+#
+# now = dt.datetime.now()
+# year = now.year
+# month = now.month
+# day_of_week = now.weekday()
+# print(day_of_week)
+#
+# date_of_birth = dt.datetime(year=1986, month=9, day=12, hour=6)
+# print(date_of_birth)
