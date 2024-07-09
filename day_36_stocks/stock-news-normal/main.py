004312259fb21dc530fb86ce3ff2f04ba0d25f72
GakuruUri <urigakuru@icloud.com>
2024-07-09 21:40:02 +0300

    Day 36 of 100 days of code.

--- day_36_stocks/stock-news-normal/main.py
+++ day_36_stocks/stock-news-normal/main.py
@@ -1,5 +1,5 @@
 import requests
-
+from twilio.rest import Client
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -8,6 +8,9 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "49MHDKNOQZ9OAHDL"
+NEWS_API_KEY = "987c45f3f3564ce8a903a8543412b953"
+TWILIO_SID = "AC33aa47f1e8756079d080312ea0bb2fbd"
+TWILIO_AUTH_TOKEN = "6b462a61133a300155282dffc94c89c2"
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -27,29 +30,42 @@ day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 print(day_before_yesterday_closing_price)
 
-difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
-print(difference)
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
 
-diff_percent = (difference / float(yesterday_closing_price)) * 100
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
 print(diff_percent)
 
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-if diff_percent > 2:
-    print("Get News.")
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+if abs(diff_percent) > 0:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+    print(articles)
 
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+    three_articles = articles[:3]
+    print(three_articles)
 
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+    formatted_articles = [
+        f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for
+        article in three_articles]
 
+    # formatted_article = [f"Headline: {article['title']}. \nBrief: {articles ['description']}"
+    #                      for article in three_articles]
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
-
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_="+18149073398",
+            to="+254708318239"
+        )
 
 
 
