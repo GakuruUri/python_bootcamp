ada7350291fc40e3d79a725b627ba72fe1ad1bbe
GakuruUri <urigakuru@icloud.com>
2024-08-23 22:30:37 +0300

    Day 47 of 100 days of code.

--- day_47_amazon/main.py
+++ day_47_amazon/main.py
@@ -2,7 +2,16 @@ from bs4 import BeautifulSoup
 import requests
 
 response = requests.get("https://appbrewery.github.io/instant_pot/")
-soup = BeautifulSoup(response.text, "html.parser")
+soup = BeautifulSoup(response.content, "html.parser")
 
-pot_price = soup.find(name="span", class_="a-offscreen")
-print(pot_price)
+price = soup.find(class_="a-offscreen").get_text()
+# print(price)
+
+#Remove dollar sign using split
+price_without_sign = price.split("$")[1]    # this is now a string
+# print(price_without_sign) 
+
+
+#Convert price without dollar sign into a float
+price_as_float = float(price_without_sign)
+print(price_as_float)
