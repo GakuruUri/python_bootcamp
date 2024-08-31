866283e907ce4ded2574e7d5dd42c32a97b7b2b5
GakuruUri <urigakuru@icloud.com>
2024-08-31 22:23:45 +0300

    Day 48 of 100 days of code.

---
+++ day_48_Selenium/upcoming_challenge.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Keep browser open
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.python.org/")
+
+
+bug_link = driver.find_element(By.XPATH, value='//*[@id="content"]/div/section/div[2]/div[2]/div/ul')
+
+tier_1 = driver.find_elements(By.CLASS_NAME, value='tier-1')
+
+
+# Challenge: print the event dates from python.org
+
+event_times = driver.find_elements(By.CSS_SELECTOR, value=".event-widget time")
+# for time in event_times:
+#     print(time.text)
+
+event_names = driver.find_elements(By.CSS_SELECTOR, value=".event-widget li a")
+# for name in event_names:
+#     print(name.text)
+
+events = {}
+
+for n in range(len(event_times)):
+    events[n] = {
+        "time": event_times[n].text,
+        "name": event_names[n].text,
+    }
+print(events)
+
+
+
+
+driver.quit()
