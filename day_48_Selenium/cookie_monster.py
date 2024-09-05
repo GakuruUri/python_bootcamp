8bcf158364b7b3cdc25152a3d67c291be12a4fa6
GakuruUri <urigakuru@icloud.com>
2024-09-05 21:10:51 +0300

    Day 48 of 100 days of code.

--- day_48_Selenium/cookie_monster.py
+++ day_48_Selenium/cookie_monster.py
@@ -1,21 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+import time
 
-# Keep chrome browser open after program finishes
+# Optional keep the browser open(Helps diagonise issue if browser crushed.
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
-# Create and configure the chrome webdriver
 driver = webdriver.Chrome(options=chrome_options)
-
-# Navigate to the cookie monster website.
-driver.get("https://orteil.dashnet.org/cookieclicker/")
-
-
-# Simulate 1000000 clicks on the cookie monster.
-
-language = driver.find_element(By.ID, "English")
-language.click()
-clicker = driver.find_element(By.ID, value="bigCookie")
-clicker.click()
